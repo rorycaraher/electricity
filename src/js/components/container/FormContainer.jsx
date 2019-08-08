4c0cda7bda83ca1e5652daf2e775b1bf2ed61562
Rory Caraher <caraher.rory@gmail.com>
2019-08-08 14:31:28 +0100

    add toggle class

--- src/js/components/container/FormContainer.jsx
+++ src/js/components/container/FormContainer.jsx
@@ -1,44 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Input from "../presentational/Input.jsx";
+import Toggle from './Toggle.jsx';
 
-function Greeting(props) {
-    return (
-        <p>{props.item}</p>
-    ) 
-}
-
-function Toggle(props) {
-    return (
-        <p onClick={props.onClick}>{props.onoff}</p>
-    ) 
-}
-class FormContainer extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            light_status: 0
-        };
-        // this.handleChange = this.handleChange.bind(this);
-    }
-
-    buttonClick() {
-        this.props.onoff = "2"
-    }
-
-    handleChange(event) {
-        this.setState({ [event.target.id]: event.target.value });
-    }
-
-
+class App extends React.Component {
     render() {
-        return (
-            <div>
-                <Button onoff="0" onClick={()=>this.buttonClick()}/>
-            </div>
-        )
+      return (
+        <div>
+          <Toggle />
+          <Toggle />
+        </div>
+      );
     }
-}
-export default FormContainer;
+  }
 
-ReactDOM.render(<FormContainer />,document.getElementById("create-article-form"));
+
+ReactDOM.render(<App />,document.getElementById("app"));
