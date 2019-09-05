c18cff175b9cf6333c549bb955547b03f8f6f7ff
Rory Caraher <caraher.rory@gmail.com>
2019-09-05 20:51:53 +0100

    configure server to work with webpack

--- server.py
+++ server.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python
 
 import os
-from flask import Flask, render_template, request
-import RPi.GPIO as GPIO
-app = Flask(__name__)
+from flask import Flask, render_template, request, url_for
+# import RPi.GPIO as GPIO
+app = Flask(__name__, template_folder='dist', static_folder="dist", static_url_path="")
 
 # GPIO setup
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
+# GPIO.setwarnings(False)
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
 
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/lights", methods=['GET'])
-def lights():
-    pin_number = request.args.get('data')
-    pin_int = int(pin_number)
-    # check if pin_number is high or low
-    # then change it to opposite!
-    GPIO.output(pin_int, 1)
-    return "switch light: " + pin_number
+# @app.route("/lights", methods=['GET'])
+# def lights():
+#     pin_number = request.args.get('data')
+#     pin_int = int(pin_number)
+#     # check if pin_number is high or low
+#     # then change it to opposite!
+#     GPIO.output(pin_int, 1)
+#     return "switch light: " + pin_number
 
-@app.route("/on")
-def on():
-    return "on"
+# @app.route("/on")
+# def on():
+#     return "on"
 
-@app.route("/off")
-def off():
-    return "off"
+# @app.route("/off")
+# def off():
+#     return "off"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get('PORT', 3000), debug=True)
